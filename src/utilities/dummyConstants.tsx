aa5218530cf4c982379b535b8e2620247ae447a3
tobe_wdw <softwaredeveloper@wdwltd.com>
2025-06-23 17:31:12 +0100

    Changed null messages for the chat

--- src/utilities/dummyConstants.tsx
+++ src/utilities/dummyConstants.tsx
@@ -1,48 +1,6 @@
 export const chat = [
   {
-    message: "Hi, how are you?",
-    sender: "User",
-  },
-  {
-    message:
-      "I am doing well. I will answer only questions specific to insurance please, thanks",
-    sender: "Bot",
-  },
-  {
-    message:
-      "I am doing well. I will answer only questions specific to insurance please, thanks",
-    sender: "Bot",
-  },
-  {
-    message:
-      "I am doing well. I will answer only questions specific to insurance please, thanks",
-    sender: "Bot",
-  },
-  {
-    message: "Hi, how are you?",
-    sender: "User",
-  },
-  {
-    message: "Hi, how are you?",
-    sender: "User",
-  },
-  {
-    message: "Hi, how are you?",
-    sender: "User",
-  },
-  {
-    message:
-      "I am doing well. I will answer only questions specific to insurance please, thanks",
-    sender: "Bot",
-  },
-  {
-    message:
-      "I am doing well. I will answer only questions specific to insurance please, thanks",
-    sender: "Bot",
-  },
-  {
-    message:
-      "I am doing well. I will answer only questions specific to insurance please, thanks",
+    message: "Hi, Good day. Please ask any questions you have about insurance",
     sender: "Bot",
   },
 ];
