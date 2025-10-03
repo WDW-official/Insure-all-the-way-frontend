75738180f89d904574e649aa797625580b8810c7
Tobe0504 <ezimorahtobenna@gmail.com>
2025-10-03 10:27:42 +0100

    Added updates by Eniola

--- src/container/HomeHowWeWork/HomeHowWeWork.tsx
+++ src/container/HomeHowWeWork/HomeHowWeWork.tsx
@@ -35,7 +35,7 @@ const HomeHowWeWork = () => {
   return (
     <section className={classes.container}>
       <div>
-        <h2>How It Works</h2>
+        <h2>One Dashboard, All your Insurance Needs</h2>
         <h4>Get Covered In 4 Easy Steps</h4>
         <p>
           We’ve made getting insurance fast and hassle-free. Follow these four
