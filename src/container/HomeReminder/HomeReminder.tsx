54d8ec37b3875fb6c45cf52c130fa28f26607489
Tobe0504 <ezimorahtobenna@gmail.com>
2025-11-04 16:05:33 +0100

    Added the new updates for reminder section

--- src/container/HomeReminder/HomeReminder.tsx
+++ src/container/HomeReminder/HomeReminder.tsx
@@ -30,17 +30,20 @@ const HomeReminder = () => {
           />
         </div>
         <div>
-          <h4>Get Renewal Reminders</h4>
+          <h4>Never miss your vehicle insurance renewal</h4>
           <p>
             Never miss your vehicle insurance renewal again. With our free
             Renewal Reminder Service, you’ll receive timely alerts before your
             insurance, vehicle licence, or roadworthiness certificate expires.
           </p>
           <p>
-            Simply enter your vehicle and contact details and Insure All The Way
-            will keep you covered and compliant.
+            Simply enter your vehicle and contact details and{" "}
+            <strong>Insure All The Way</strong> will keep you covered and
+            compliant.
           </p>
-          <p>Insurance Made Easy.</p>
+          <p>
+            <strong>{"Insurance Made Easy".toUpperCase()} </strong>
+          </p>
           <Button
             type="secondary"
             onClick={() => setModalTrue(setModals, "reminder")}
