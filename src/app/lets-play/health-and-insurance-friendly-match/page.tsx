f124c03fc566e2c662c1ea35749daff1f932d85d
snr-dev-wdw <softwaredeveloperwdwltd.com>
2026-01-12 16:44:37 +0100

    Cleared unused var

--- src/app/lets-play/health-and-insurance-friendly-match/page.tsx
+++ src/app/lets-play/health-and-insurance-friendly-match/page.tsx
@@ -1,6 +1,5 @@
 import Loader from "@/components/Loader/Loader";
 import HealthAndInsuranceFriendlyMatch from "@/container/HealthAndInsuranceFriendlyMatch/HealthAndInsuranceFriendlyMatch";
-import ApppLayout from "@/layouts/ApppLayout/ApppLayout";
 import React, { Suspense } from "react";
 
 const page = () => {
